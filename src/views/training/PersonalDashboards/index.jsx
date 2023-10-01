cd94f9fcc8bd60e393a54f28d2a6e7141306892a
Pavel <pavel-zh@inbox.ru>
2023-10-01 11:00:40 +0300

    first analytics

---
+++ src/views/training/PersonalDashboards/index.jsx
@@ -0,0 +1,163 @@
+import {
+    CButton,
+    CCard,
+    CCardBody,
+    CCardGroup,
+    CCol,
+    CContainer,
+    CForm,
+    CFormInput,
+    CInputGroup,
+    CInputGroupText,
+    CRow,
+    CProgress
+} from '@coreui/react'
+
+import {
+    cibCcAmex,
+    cibCcApplePay,
+    cibCcMastercard,
+    cibCcPaypal,
+    cibCcStripe,
+    cibCcVisa,
+    cibGoogle,
+    cibFacebook,
+    cibLinkedin,
+    cifBr,
+    cifEs,
+    cifFr,
+    cifIn,
+    cifPl,
+    cifUs,
+    cibTwitter,
+    cilCloudDownload,
+    cilPeople,
+    cilUser,
+    cilUserFemale,
+} from '@coreui/icons'
+
+import CIcon from '@coreui/icons-react'
+
+import { getFormatDate } from '../../../utils/datetime.js';
+
+
+export const PersonalDashboard = ({ myActivityStat }) => {
+
+
+
+    const progressGroupExample1 = [
+        { title: '25.09.2023', value1: 34, value2: 78 },
+        { title: '24.09.2023', value1: 56, value2: 94 },
+        { title: '23.09.2023', value1: 12, value2: 67 },
+        { title: '22.09.2023', value1: 43, value2: 91 },
+        { title: '21.09.2023', value1: 22, value2: 73 },
+        { title: '20.09.2023', value1: 53, value2: 82 },
+        { title: '19.09.2023', value1: 9, value2: 69 },
+    ]
+
+    const progressGroupExample2 = [
+        { title: 'Male', icon: cilUser, value: 53 },
+        { title: 'Female', icon: cilUserFemale, value: 43 },
+    ]
+
+    const progressGroupExample3 = [
+        { title: 'Organic Search', icon: cibGoogle, percent: 56, value: '191,235' },
+        { title: 'Facebook', icon: cibFacebook, percent: 15, value: '51,223' },
+        { title: 'Twitter', icon: cibTwitter, percent: 11, value: '37,564' },
+        { title: 'LinkedIn', icon: cibLinkedin, percent: 8, value: '27,319' },
+    ]
+
+    return (
+        <div>
+            <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', gap: "15px" }}>
+                {/* <CRow> */}
+                <CCol xs={12} md={6} xl={12}>
+                    <CRow>
+                        <CCol sm={4}>
+                            <div className="border-start border-start-4 border-start-info py-1 px-3">
+                                <div className="text-medium-emphasis small">Активность</div>
+                                <div className="fs-5 fw-semibold">{myActivityStat.activity_title}</div>
+                            </div>
+                        </CCol>
+                        <CCol sm={4}>
+                            <div className="border-start border-start-4 border-start-danger py-1 px-3 mb-3">
+                                <div className="text-medium-emphasis small">{myActivityStat.activity_unit_label}</div>
+                                <div className="fs-5 fw-semibold">17 {myActivityStat.activity_unit}</div>
+                            </div>
+                        </CCol>
+                        <CCol sm={4}>
+                            <div className="border-start border-start-4 border-start-danger py-1 px-3 mb-3">
+                                <div className="text-medium-emphasis small">Баллов</div>
+                                <div className="fs-5 fw-semibold">{myActivityStat.sum_cost_event_in_units}</div>
+                            </div>
+                        </CCol>
+                    </CRow>
+
+                    <hr className="mt-0" />
+                    {myActivityStat.results.map((item, index) => (
+                        <div className="progress-group mb-4" key={index}>
+                            <div className="progress-group-prepend">
+                                <span className="text-medium-emphasis small">{getFormatDate(item.start_datetime)}</span>
+                            </div>
+                            <div className="progress-group-bars">
+                                <CProgress thin color="info" value={item.cost_event_in_units / myActivityStat.sum_cost_event_in_units * 100} />
+                                <CProgress thin color="danger" value={(myActivityStat.sum_cost_event_in_units / 30 * 100) / myActivityStat.results.length} />
+                            </div>
+                        </div>
+                    ))}
+                </CCol>
+                {/* <CCol xs={4} md={4} xl={4}>
+                    <CRow>
+                        <CCol sm={4}>
+                            <div className="border-start border-start-4 border-start-warning py-1 px-3 mb-3">
+                                <div className="text-medium-emphasis small">Pageviews</div>
+                                <div className="fs-5 fw-semibold">78,623</div>
+                            </div>
+                        </CCol>
+                        <CCol sm={4}>
+                            <div className="border-start border-start-4 border-start-success py-1 px-3 mb-3">
+                                <div className="text-medium-emphasis small">Organic</div>
+                                <div className="fs-5 fw-semibold">49,123</div>
+                            </div>
+                        </CCol>
+                    </CRow>
+
+                    <hr className="mt-0" />
+
+                    {progressGroupExample2.map((item, index) => (
+                        <div className="progress-group mb-4" key={index}>
+                            <div className="progress-group-header">
+                                <CIcon className="me-2" icon={item.icon} size="lg" />
+                                <span>{item.title}</span>
+                                <span className="ms-auto fw-semibold">{item.value}%</span>
+                            </div>
+                            <div className="progress-group-bars">
+                                <CProgress thin color="warning" value={item.value} />
+                            </div>
+                        </div>
+                    ))}
+
+                    <div className="mb-5"></div>
+
+                    {progressGroupExample3.map((item, index) => (
+                        <div className="progress-group" key={index}>
+                            <div className="progress-group-header">
+                                <CIcon className="me-2" icon={item.icon} size="lg" />
+                                <span>{item.title}</span>
+                                <span className="ms-auto fw-semibold">
+                                    {item.value}{' '}
+                                    <span className="text-medium-emphasis small">({item.percent}%)</span>
+                                </span>
+                            </div>
+                            <div className="progress-group-bars">
+                                <CProgress thin color="success" value={item.percent} />
+                            </div>
+                        </div>
+                    ))}
+                </CCol> */}
+                {/* </CRow> */}
+            </div>
+
+        </div >
+    );
+};
--- src/views/training/Training.jsx
+++ src/views/training/Training.jsx
@@ -78,6 +78,7 @@ import {
 } from '@coreui/icons'
 
 import { getFormatDate } from '../../utils/datetime.js';
+import { PersonalDashboard } from "./PersonalDashboards";
 
 // import { every } from 'core-js/core/array'
 
@@ -92,8 +93,73 @@ const serviceApiPromise = (method = 'get', full_url = '', data = {}, headers = {
 const Training = (props) => { // настройки приложения
     const { startUrlApi, userData, getHeaders } = useContext(AppContext);
 
+    // Данные из профиля пользователя
+    const [dataProfile, setDataProfile] = useState(userData)
+    const [myActivityStatList, setmyActivityStatList] = useState([
+        {
+            results: []
+        }
+    ]
+    )
+
+    // Загружаем данные аналитики
+    async function loadDataService(url) {
+        return axios({ url: `${startUrlApi}${url}`, method: 'get', headers: getHeaders() }).then(req => req.data);
+    }
+
+    async function postData(url, dataJson) {
+        return axios({ url: `${startUrlApi}${url}`, method: 'post', headers: getHeaders(), data: dataJson }).then(req => req.data);
+    }
+
+    //  # 1 Данные моей активности
+    const dataMyActiviry = async (period = null) => {
+        const url = period ? `/api/analytics/tracker/my/?period=${period}` : `/api/analytics/tracker/my/`;
+        const data = await loadDataService(url);
+        setmyActivityStatList(data);
+    }
+    const dataMyEvents = async (period = null) => {
+        const url = period ? `/api/analytics/tracker/my/?period=${period}` : `/api/analytics/tracker/my/`;
+        const data = await loadDataService(url);
+        setmyActivityStatList(data);
+    }
+
+    // # 2 События
+    const dataEvents = async (status = 'new') => {
+        const url = status ? `/api/events/all/?status=${status}` : `/api/events/all/`;
+        const data = await loadDataService(url);
+        setEventsDataList(data);
+    }
+
+
+
+
+
+    useEffect(() => {
+        setDataProfile(userData);
+        dataMyActiviry(7);
+        // dataEvents('new')
+        // loadData();
+
+    }, [])
+
     return (
-        <div>Статистика по трениировкам</div>
+        <div className="content_container">
+            <CCard className="mb-4">
+
+                <CCardBody>
+                    <CRow className='margin-top'>Выберите период детализации:
+                    </CRow>
+                    <CRow className='margin-top'>
+
+                        {
+                            myActivityStatList.map((item) =>
+                                <PersonalDashboard myActivityStat={item} />
+                            )
+                        }
+                    </CRow>
+                </CCardBody>
+            </CCard>
+        </div>
     )
 }
 
